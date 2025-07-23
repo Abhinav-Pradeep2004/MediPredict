47280c9aa0db5b6ff301200ffe43f7f563740c25
Abhinav Pradeep <abhinavpradeep.kerala@gmail.com>
2025-07-23 22:48:31 +0530

    Update config.js

--- frontend/firebase/config.js
+++ frontend/firebase/config.js
@@ -4,13 +4,7 @@ import { getAuth } from 'firebase/auth';
 
 // Your Firebase project config (replace with your own)
 const firebaseConfig = {
-    apiKey: "AIzaSyBPTF-18-78GBhbSkUinyLJ1QeWvcJEHBM",
-    authDomain: "medipredict-46e3c.firebaseapp.com",
-    projectId: "medipredict-46e3c",
-    storageBucket: "medipredict-46e3c.firebasestorage.app",
-    messagingSenderId: "1018444268776",
-    appId: "1:1018444268776:web:e8cc79c52b1ab4de33f55d",
-    measurementId: "G-DN831KECVX"
+"Your Firebase API"
   };
 
 const app = initializeApp(firebaseConfig);
